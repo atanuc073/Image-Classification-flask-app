5f53e19065eda61cc297186b3a036854fc1eed01
atanuc073 <atanuc073@gmail.com>
2021-07-09 20:26:15 +0530

    flask app html

---
+++ flask1_app/flask_app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template,Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
